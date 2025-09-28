7daaa6e0dfa028f1e3d76df0e5923af74033e7ed
Eva_60301251 <60301251@udst.edu.qa>
2025-09-28 08:48:07 +0300

    func: getPhoto

--- business_layer.js
+++ business_layer.js
@@ -4,3 +4,19 @@
 * INFS3201-5/6- Web Tech 2 
 * Assignment 2
 */
+
+const{
+    loadPhoto,
+    loadAlbum,
+    savePhoto,
+    saveAlbum,
+    findPhoto,
+    findAlbum,
+    findAlbumbyName,
+} = require('./persistance_layer')
+
+//Get photo details by ID
+async fuction getPhoto(photoID){
+    return await findPhoto(photoID)
+}
+
