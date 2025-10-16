d46a790ece7fb87e215a39af461228aec675321e
Eva_60301251 <60301251@udst.edu.qa>
2025-10-17 01:48:16 +0300

    list of albums

---
+++ routes/handlers.js
@@ -0,0 +1,21 @@
+/**
+* Name:Eva Paul
+* Student ID:60301251
+* INFS3201-5/6- Web Tech 2 
+* Assignment 3
+*/
+
+const express= require('express')
+const router=express.Router()
+const{
+    getPhoto,
+    getAlbum,
+    updatePhoto,
+    getByAlbum,
+    addTag
+}= require('../business_layer')
+
+router.get('/', async(req,res)=>{
+    const albums=await require('../persistance_layer').loadAlbum()
+    res.render('index' , {albums, layout: undefined})
+})
