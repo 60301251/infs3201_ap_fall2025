8cf71beabb6631c68a9c24429201d71282dae863
Eva_60301251 <60301251@udst.edu.qa>
2025-10-17 01:37:26 +0300

    htmlpresentation

---
+++ htmlpresentation.js
@@ -0,0 +1,28 @@
+/**
+* Name:Eva Paul
+* Student ID:60301251
+* INFS3201-5/6- Web Tech 2 
+* Assignment 3
+*/
+
+
+const express= require('express')
+const path=require('path')
+const handlebars= require('express-handlebars')
+const bodyParser=require('body-parser')
+const routes= require('./routes/handlers')
+
+const app=express()
+const PORT=3000
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use('/photos', express.static(path.join(__dirname,'photos')))
+app.use('/public', express.static(path.join(__dirname,'public')))
+
+app.engine('handlebars',handlebars.engine({layoutDir: undefined}))
+app.set('view engine','handlebars')
+app.set('views',path.join(__dirname,'templates'))
+
+app.use('/',routes)
+
+app.listen(PORT,()=> console.log('Server running at http://localhost:${PORT}'))
