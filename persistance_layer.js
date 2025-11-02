c3845079f3d78e2b3be650d54d8864e19ce28b1a
Eva_60301251 <60301251@udst.edu.qa>
2025-11-02 09:07:05 +0300

    func verfiyPassword

--- persistance_layer.js
+++ persistance_layer.js
@@ -23,6 +23,12 @@ async function connectDatabase(){
 function hashPassword(password){
     const salt = crypto.randomBytes(16).toString('hex')
     const hash= crypto.pbkdf2Sync(password,salt,1000,64,'sha512').toString('hex')
+    return {salt, hash}
+}
+
+function verifyPassword(password, salt,storedHash){
+    const hash= crypto.pbkdf2Sync(password,salt,1000,64,'sha512').toString('hex')
+    return hash == storedHash
 }
 
 /**
